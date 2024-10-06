a492555bf311456df9815fbcb86b05edcb041b76
verobelo <142001535+verobelo@users.noreply.github.com>
2024-10-06 19:10:39 +0200

    Add favorites script

---
+++ script/favorites.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const favoritesCount = document.querySelector(".header__favorites-count");
+  const favoritesDropdown = document.querySelector(
+    ".header__favorites-dropdown"
+  );
+  const favorites = JSON.parse(sessionStorage.getItem("favorites")) || [];
+  updateFavoritesCount();
+
+  document.querySelectorAll(".infosheet__favorites").forEach((button) => {
+    button.addEventListener("click", function () {
+      const animal = {
+        name: this.dataset.name,
+        image: this.dataset.image,
+      };
+      addAnimalToFavorites(animal);
+    });
+  });
+
+  document.querySelector(".header__favorites").addEventListener("click", () => {
+    favoritesDropdown.classList.toggle("show");
+    displayFavorites();
+  });
+
+  function addAnimalToFavorites(animal) {
+    favorites.push(animal);
+    sessionStorage.setItem("favorites", JSON.stringify(favorites));
+    updateFavoritesCount();
+  }
+
+  function updateFavoritesCount() {
+    favoritesCount.textContent = favorites.length;
+  }
+
+  function displayFavorites() {
+    favoritesDropdown.innerHTML = "";
+    favorites.forEach((animal) => {
+      favoritesDropdown.innerHTML += `
+                <div class="favorite-item">
+                    <img src="${animal.image}" alt="${animal.name}" />
+                    <p>${animal.name}</p>
+                    <button class="infosheet__button">Оформить заявку</button>
+                </div>
+            `;
+    });
+  }
+});
