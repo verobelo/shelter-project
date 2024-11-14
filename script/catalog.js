0afd5fefca4fd3964ef10ecf8163576f7b7fe851
verobelo <142001535+verobelo@users.noreply.github.com>
2024-11-14 15:02:58 +0100

    Chnage gender naming

--- script/catalog.js
+++ script/catalog.js
@@ -126,7 +126,7 @@ function displayCards(page, data = fetchedData) {
   catalogCardGroup.innerHTML = "";
 
   pageData.forEach((pet) => {
-    const genderSymbol = pet.gender === "Мужской" ? "&#9794;" : "&#9792;";
+    const genderSymbol = pet.gender === "Мальчик" ? "&#9794;" : "&#9792;";
     const petDetails = `${pet.id}-details.html`;
 
     console.log(pet.gender);
